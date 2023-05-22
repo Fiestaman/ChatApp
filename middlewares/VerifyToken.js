bf45d143d80109ac93ef9ccda1b325cf5c95f4fd
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-05-22 15:04:04 -0500

    add VerifyToken middleware

---
+++ middlewares/VerifyToken.js
@@ -0,0 +1,34 @@
+import auth from "../config/firebase-config.js";
+
+export const VerifyToken = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  try {
+    const decodeValue = await auth.verifyIdToken(token);
+    if (decodeValue) {
+      req.user = decodeValue;
+      return next();
+    } else {
+      req.user = null;
+      return next();
+    }
+  } catch (e) {
+    return res.json({ message: "Internal Error" });
+  }
+};
+
+export const VerifySocketToken = async (socket, next) => {
+  const token = socket.handshake.auth.token;
+
+  try {
+    const decodeValue = await auth.verifyIdToken(token);
+
+    if (decodeValue) {
+      socket.user = decodeValue;
+
+      return next();
+    }
+  } catch (e) {
+    return next(new Error("Internal Error"));
+  }
+};
