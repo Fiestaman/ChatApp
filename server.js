ab07b5b218b49756bf4f301d49a6cda629fa5f95
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-05-23 18:16:31 -0500

    define __dirname

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import fileURLToPath from "url";
+import { fileURLToPath } from "url";
 import { Server } from "socket.io";
 
 import "./config/database.js";
