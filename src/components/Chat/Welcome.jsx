db0863b5cf396855bf1296e145dd865f6ae556d0
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-05-23 16:26:57 -0500

    add welcome image

--- src/components/Chat/Welcome.jsx
+++ src/components/Chat/Welcome.jsx
@@ -1,10 +1,10 @@
-// import { WelcomeSVG } from "../../utils/WelcomeSVG";
+import { WelcomeSVG } from "../../utils/Welcome";
 
 export default function Welcome() {
   return (
     <div className="lg:col-span-2 lg:block bg-white dark:bg-gray-900">
       <div className="pl-5">
-        {/* <WelcomeSVG /> */}
+        <WelcomeSVG className="w-full" />
         <div className="text-center">
           <h2 className="text-xl text-gray-500 dark:text-gray-400">
             Select a Chat to Start Messaging
